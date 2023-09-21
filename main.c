46d2a935f339fc12bc09e68530ef170c2d27206e
yeon3ee <2310538@sookmyung.ac.kr>
2023-09-21 16:36:51 +0900

    4

--- main.c
+++ main.c
@@ -4,14 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
+	int a,b;
 	
-	printf("input Alphabet: ");
-	scanf("%c",&c);
+	printf("a나누기 b를 하겠습니다.\n");
+	printf("a를 입력하세요 : ");
+	scanf("%d",&a);
 	
-	printf("input Alphabet: %c\n", c);
+	printf("b를 입력하세요 : ");
+	scanf("%d", &b);
 	
-	char nextc = c+1;
-	printf("next Alphabet : %c\n", nextc);
+	float result =(float) a/b;
+	printf("나누기의 결과는 %f 입니다.\n", result);
 	return 0;
 }
